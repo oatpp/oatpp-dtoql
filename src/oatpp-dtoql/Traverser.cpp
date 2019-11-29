74d8b72cc8c89246f168b6ba36c9320c7be4aeed
lganzzzo <lganzzzo@gmail.com>
2019-11-29 04:26:32 +0200

    Fields selection for Traverser

---
+++ src/oatpp-dtoql/Traverser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leonid  on 2019-11-29.
+//
+
+#include "Traverser.hpp"
---
+++ src/oatpp-dtoql/Traverser.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Leonid  on 2019-11-29.
+//
+
+#ifndef OATPP_DTOQL_TRAVERSER_HPP
+#define OATPP_DTOQL_TRAVERSER_HPP
+
+
+class Traverser {
+
+};
+
+
+#endif //OATPP_DTOQL_TRAVERSER_HPP
